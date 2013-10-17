86a0a9c153ebca63a7af66d3f5543e06e1a425f0
Andrew Mass <amass1212@gmail.com>
2013-10-17 18:42:29 -0500

    Updates overall style and provides initial design for homepage.

--- index.php
+++ index.php
@@ -11,58 +11,63 @@
     <?php include './head.php'; ?>
   </head>
 
-  <body>
+  <body class="home">
     <?php include './header.php'; ?>
 
-    <div>
-      <img src="./img/drivingscaledplaceholder.jpg" width=100% id="main">
+    <img id="hero-main" src="./img/hero-main.jpg" id="main">
 
-      <div class="content container">
-        <div class="row">
-          <div class="col-md-8">
-            <h2>What is Illini Motorsports?</h2>
+    <div class="content container">
+      <div class="row">
+        <div class="col-md-8">
+          <h1>Illini Motorsports</h1>
 
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean sed dictum lacus. Suspendisse augue erat, feugiat posuere ullamcorper vel, ultrices vel magna. Integer rutrum laoreet facilisis. Aliquam consequat cursus risus, quis semper augue blandit vitae. Nulla vestibulum eros ac velit ultrices, id lacinia lectus varius. Morbi malesuada nibh libero, a cursus felis dapibus nec. Duis at porta lectus. Quisque quis diam non nunc molestie egestas. Praesent bibendum ultrices arcu, vel vehicula arcu laoreet vel. Quisque sed nunc porta, commodo sem quis, aliquet massa. Curabitur faucibus rutrum leo, ut pellentesqueque est bibendum sed.
+          <p>The collegiate chapter of the Society of Automotive Engineers (SAE) at the University of
+Illinois aims to provide its members with numerous educational, professional, and social
+opportunities related to vehicular design. As part of this initiative, UIUC fields the Formula
+SAE team.</p>
 
-            <br>
+          <p>Each year, Formula SAE teams are challenged by a fictional manufacturing company to
+design and develop a small formula-style racing car targeted at the nonprofessional
+weekend autocross enthusiast. Based on a predetermined set of templates and rules,
+teams must design the fastest, most effective racing machine possible while minimizing
+costs, maximizing reliability, and utilizing the latest technologies in racing today.
+C ompetitions are held annually in various places all over the world (Michigan, Nebraska,
+Australia, Brazil, Italy, Germany, etc.) where these design teams bring their car and
+compete against some of the best engineering students worldwide.</p>
 
-            Aenean tincidunt nisi et elementum auctor. Sed fringilla magna eu massa sollicitudin ultrices. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Quisque ornare porttitor interdum. Phasellus eu purus gravida, fermentum massa faucibus, venenatis lorem. Sed non orci a massa tristique interdum vel et sem. Aenean sagittis, est in mattis vulputate, ante nibh auctor massa, at imperdiet augue neque at ligula. Pellentesque tristique ipsum id turpis fermentum convallis. Proin dignissim hendrerit tempus. Integer eu libero pellentesque, varius sem vitae, venenatis tellus. In vel vestibulum est, lobortis molestie lorem. Sed at urna auctor, sollicitudin justo quis, viverra ante. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.
-          </div>
+          <img src="./img/rendered.jpg">
+        </div>
 
-          <div class="col-md-4">
-            <a class="twitter-timeline" href="https://twitter.com/IllinoisFSAE" data-widget-id="383104434047180800">
-              Tweets by @IllinoisFSAE
-            </a>
+        <div id="twitter-box" class="col-md-4">
+          <a class="twitter-timeline" href="https://twitter.com/IllinoisFSAE" data-widget-id="383104434047180800"></a>
 
-            <script>
-              !function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+"://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");
-            </script>
+          <script>
+            !function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+"://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");
+          </script>
 
-            FACEBOOK
-
-          </div>
+          <!-- FACEBOOK -->
         </div>
       </div>
+    </div>
 
-      <div class="content container">
-        <hr>
+    <div class="content container">
+      <hr>
 
-        <div class="row">
-          <div class="col-md-3">
-            <img src="./img/logos/mechse.jpg">
-          </div>
+      <div class="row">
+        <div class="col-md-3">
+          <img src="./img/logos/mechse.jpg">
+        </div>
 
-          <div class="col-md-3">
-            <img src="./img/logos/cat.jpg">
-          </div>
+        <div class="col-md-3">
+          <img src="./img/logos/cat.jpg">
+        </div>
 
-          <div class="col-md-3">
-            <img src="./img/logos/bosch.jpg">
-          </div>
+        <div class="col-md-3">
+          <img src="./img/logos/bosch.jpg">
+        </div>
 
-          <div class="col-md-3">
-            <img src="http://placehold.it/200x200">
-          </div>
+        <div class="col-md-3">
+          <img src="http://placehold.it/200x200">
         </div>
       </div>
     </div>
--- sponsors.php
+++ sponsors.php
@@ -14,164 +14,154 @@
   <body class="sponsors">
     <?php include './header.php'; ?>
 
-    <div>
+    <div class="content container">
+
+      <!--<h4>Want to see your company's logo here? <a href="./img/sponsor.pdf" target="_blank">Sponsor Us</a></h4>
+      -->
       <div class="content container">
-        <h2>Sponsorship</h2>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean sed dictum lacus. Suspendisse augue erat, feugiat posuere ullamcorper vel, ultrices vel magna. Integer rutrum laoreet facilisis. Aliquam consequat cursus risus, quis semper augue blandit vitae. Nulla vestibulum eros ac velit ultrices, id lacinia lectus varius. Morbi malesuada nibh libero, a cursus felis dapibus nec. Duis at porta lectus. Quisque quis diam non nunc molestie egestas. Praesent bibendum ultrices arcu, vel vehicula arcu laoreet vel. Quisque sed nunc porta, commodo sem quis, aliquet massa. Curabitur faucibus rutrum leo, ut pellentesqueque est bibendum sed.
+        <h1>Orange and Blue Partners</h1>
 
-        <br>
-
-        Aenean tincidunt nisi et elementum auctor. Sed fringilla magna eu massa sollicitudin ultrices. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Quisque ornare porttitor interdum. Phasellus eu purus gravida, fermentum massa faucibus, venenatis lorem. Sed non orci a massa tristique interdum vel et sem. Aenean sagittis, est in mattis vulputate, ante nibh auctor massa, at imperdiet augue neque at ligula. Pellentesque tristique ipsum id turpis fermentum convallis. Proin dignissim hendrerit tempus. Integer eu libero pellentesque, varius sem vitae, venenatis tellus. In vel vestibulum est, lobortis molestie lorem. Sed at urna auctor, sollicitudin justo quis, viverra ante. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.
-
-        <br>
-
-        Sponsor Us!
-
-        <div class="content container">
-          <hr>
-
-          <h1>Orange and Blue Partners</h1>
-
-          <div class="row">
-            <div class="col-md-6">
+        <div class="row">
+          <div class="col-md-6">
+            <a href="//www.google.com" target="_blank">
               <img src="./img/logos/mechse.jpg">
-            </div>
-
-            <div class="col-md-6">
-              <img src="./img/logos/cat.jpg">
-            </div>
+            </a>
           </div>
 
-          <div class="row">
-            <div class="col-md-6">
-              <img src="./img/logos/bosch.jpg">
-            </div>
+          <div class="col-md-6">
+            <img src="./img/logos/cat.jpg">
+          </div>
+        </div>
 
-            <div class="col-md-6">
-              <img src="http://placehold.it/400x250">
-            </div>
+        <div class="row">
+          <div class="col-md-6">
+            <img src="./img/logos/bosch.jpg">
           </div>
 
-          <hr>
+          <div class="col-md-6">
+            <img src="http://placehold.it/400x250">
+          </div>
+        </div>
 
-          <h1>Gold Partners</h1>
+        <hr>
 
-          <div class="row">
-            <div class="col-md-3">
-              <img src="http://placehold.it/250x150">
-            </div>
+        <h1>Gold Partners</h1>
 
-            <div class="col-md-3">
-              <img src="http://placehold.it/250x150">
-            </div>
-
-            <div class="col-md-3">
-              <img src="http://placehold.it/250x150">
-            </div>
-
-            <div class="col-md-3">
-              <img src="http://placehold.it/250x150">
-            </div>
+        <div class="row">
+          <div class="col-md-3">
+            <img src="http://placehold.it/250x150">
           </div>
 
-          <div class="row">
-            <div class="col-md-3">
-              <img src="http://placehold.it/250x150">
-            </div>
-
-            <div class="col-md-3">
-              <img src="http://placehold.it/250x150">
-            </div>
-
-            <div class="col-md-3">
-              <img src="http://placehold.it/250x150">
-            </div>
-
-            <div class="col-md-3">
-              <img src="http://placehold.it/250x150">
-            </div>
+          <div class="col-md-3">
+            <img src="http://placehold.it/250x150">
           </div>
 
-          <hr>
-
-          <h1>Other Partners</h1>
-
-          <div class="row">
-            <div class="col-md-2">
-              <img src="http://placehold.it/150x100">
-            </div>
-
-            <div class="col-md-2">
-              <img src="http://placehold.it/150x100">
-            </div>
-
-            <div class="col-md-2">
-              <img src="http://placehold.it/150x100">
-            </div>
-
-            <div class="col-md-2">
-              <img src="http://placehold.it/150x100">
-            </div>
-
-            <div class="col-md-2">
-              <img src="http://placehold.it/150x100">
-            </div>
-
-            <div class="col-md-2">
-              <img src="http://placehold.it/150x100">
-            </div>
+          <div class="col-md-3">
+            <img src="http://placehold.it/250x150">
           </div>
 
-          <div class="row">
-            <div class="col-md-2">
-              <img src="http://placehold.it/150x100">
-            </div>
+          <div class="col-md-3">
+            <img src="http://placehold.it/250x150">
+          </div>
+        </div>
 
-            <div class="col-md-2">
-              <img src="http://placehold.it/150x100">
-            </div>
-
-            <div class="col-md-2">
-              <img src="http://placehold.it/150x100">
-            </div>
-
-            <div class="col-md-2">
-              <img src="http://placehold.it/150x100">
-            </div>
-
-            <div class="col-md-2">
-              <img src="http://placehold.it/150x100">
-            </div>
-
-            <div class="col-md-2">
-              <img src="http://placehold.it/150x100">
-            </div>
+        <div class="row">
+          <div class="col-md-3">
+            <img src="http://placehold.it/250x150">
           </div>
 
-          <div class="row">
-            <div class="col-md-2">
-              <img src="http://placehold.it/150x100">
-            </div>
+          <div class="col-md-3">
+            <img src="http://placehold.it/250x150">
+          </div>
 
-            <div class="col-md-2">
-              <img src="http://placehold.it/150x100">
-            </div>
+          <div class="col-md-3">
+            <img src="http://placehold.it/250x150">
+          </div>
 
-            <div class="col-md-2">
-              <img src="http://placehold.it/150x100">
-            </div>
+          <div class="col-md-3">
+            <img src="http://placehold.it/250x150">
+          </div>
+        </div>
 
-            <div class="col-md-2">
-              <img src="http://placehold.it/150x100">
-            </div>
+        <hr>
 
-            <div class="col-md-2">
-              <img src="http://placehold.it/150x100">
-            </div>
+        <h1>Other Partners</h1>
 
-            <div class="col-md-2">
-              <img src="http://placehold.it/150x100">
-            </div>
+        <div class="row">
+          <div class="col-md-2">
+            <img src="http://placehold.it/150x100">
+          </div>
+
+          <div class="col-md-2">
+            <img src="http://placehold.it/150x100">
+          </div>
+
+          <div class="col-md-2">
+            <img src="http://placehold.it/150x100">
+          </div>
+
+          <div class="col-md-2">
+            <img src="http://placehold.it/150x100">
+          </div>
+
+          <div class="col-md-2">
+            <img src="http://placehold.it/150x100">
+          </div>
+
+          <div class="col-md-2">
+            <img src="http://placehold.it/150x100">
+          </div>
+        </div>
+
+        <div class="row">
+          <div class="col-md-2">
+            <img src="http://placehold.it/150x100">
+          </div>
+
+          <div class="col-md-2">
+            <img src="http://placehold.it/150x100">
+          </div>
+
+          <div class="col-md-2">
+            <img src="http://placehold.it/150x100">
+          </div>
+
+          <div class="col-md-2">
+            <img src="http://placehold.it/150x100">
+          </div>
+
+          <div class="col-md-2">
+            <img src="http://placehold.it/150x100">
+          </div>
+
+          <div class="col-md-2">
+            <img src="http://placehold.it/150x100">
+          </div>
+        </div>
+
+        <div class="row">
+          <div class="col-md-2">
+            <img src="http://placehold.it/150x100">
+          </div>
+
+          <div class="col-md-2">
+            <img src="http://placehold.it/150x100">
+          </div>
+
+          <div class="col-md-2">
+            <img src="http://placehold.it/150x100">
+          </div>
+
+          <div class="col-md-2">
+            <img src="http://placehold.it/150x100">
+          </div>
+
+          <div class="col-md-2">
+            <img src="http://placehold.it/150x100">
+          </div>
+
+          <div class="col-md-2">
+            <img src="http://placehold.it/150x100">
           </div>
         </div>
       </div>
