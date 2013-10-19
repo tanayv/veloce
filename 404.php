462429578e139a4a4dd27041ef406fe92ac9ca6f
nickofbh <nickofbh@gmail.com>
2013-10-18 23:50:06 -0500

    404
    added a 404 page

---
+++ 404.php
@@ -0,0 +1,24 @@
+<?php
+  /**
+  * PHP - 404
+  * Authors - Andrew Mass, Nick Kortendick
+  */
+?>
+
+<!DOCTYPE html>
+<html>
+  <head>
+    <?php include './head.php'; ?>
+  </head>
+
+  <body class="404">
+    <?php include './header.php'; ?>
+
+    <div class="content container">
+    <h3> Oops. You shouldn't have ended up here. </h3>
+    Sorry, about that. Click <a href="./contact.php">here </a>to report the issue;
+    </div>
+
+    <?php include './footer.php'; ?>
+  </body>
+</html>
