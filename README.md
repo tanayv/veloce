8c12bf463a97a36d4d6fc20c9219f6060835b5f0
nickofbh <nickofbh@gmail.com>
2013-10-20 14:08:01 -0500

    Updated descriptions

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 veloce
 ======
 
-Website for UIUC FSAE
+The source code for the official website of the University of Illinois' Formula SAE team. 
 
 http://www.motorsports.illinois.edu/
--- php/head.php
+++ php/head.php
@@ -7,13 +7,12 @@
 
 <title>Illinois Motorsports</title>
 
-<meta name="description" content="The Official Page of UIUC's FSAE Team">
+<meta name="description" content="The Official Page for the University of Illinois' Formula SAE Team.">
 <meta name="author" content="Andrew Mass, Nick Kortendick">
 <meta http-equiv="content-type" content="text/html;charset=UTF-8">
-<meta property="og:title" content="" />
+<meta property="og:title" content="Illinois FSAE" />
 <meta property="og:url" content="//motorsports.illinois.edu/" />
-<meta property="og:image" content="//" />
-<meta property="og:description" content="">
+<meta property="og:image" content="//motorsports.illinois.edu/img/rendered.jpg" />
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
 <link rel="stylesheet" href="./style.css">
@@ -24,3 +23,13 @@
 <script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
 <script src="//ajax.googleapis.com/ajax/libs/jqueryui/1.10.3/jquery-ui.min.js"></script>
 <script src="./all.min.js"></script>
+<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-36587802-2', 'illinois.edu');
+  ga('send', 'pageview');
+
+</script>
