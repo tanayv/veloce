a6757f5d2d4c2c5d56b81051d528454d7b06806f
nickofbh <nickofbh@gmail.com>
2013-10-20 13:46:59 -0500

    Added Links

--- php/topsponsors.php
+++ php/topsponsors.php
@@ -3,25 +3,25 @@
 
       <div class="row">
         <div class="col-md-3">
-          <a href="//">
+          <a target="_blank" href="//www.ingersoll.com/">
             <img src="./img/logos/ingersoll400x200.jpg">
           </a>
         </div>
 
         <div class="col-md-3">
-          <a href="//">
+          <a target="_blank" href="//engineering.illinois.edu/">
             <img src="./img/logos/edc400x200.jpg">
           </a>
         </div>
 
         <div class="col-md-3">
-          <a href="//">
+          <a target="_blank" href="//www.3m.com">
             <img src="./img/logos/3m400x200.jpg">
           </a>
         </div>
 
         <div class="col-md-3">
-          <a href="//">
+          <a target="_blank" href="//www.generalplastics.com">
             <img src="./img/logos/generalplastics400x200.jpg">
           </a>
         </div>
