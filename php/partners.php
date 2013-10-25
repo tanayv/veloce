2d8d725e9445c4580ab7763347bbcd4f181500e8
Nicholas Kortendick <nick@nickkortendick.com>
2013-10-25 14:21:31 -0500

    Partners is more mobile friendly

--- php/partners.php
+++ php/partners.php
@@ -39,13 +39,13 @@
         <h1>Gold Partners</h1>
 
         <div class="row gold">
-          <div class="col-lg-6 col-md-6 col-sm-12 col-xs-12">
+          <div class="col-lg-6 col-md-6 col-sm-6 col-xs-6">
             <a href="//www.3m.com" target="_blank">
               <img src="./img/logos/gold/3m.jpg">
             </a>
           </div>
 
-          <div class="col-lg-6 col-md-6 col-sm-12 col-xs-12">
+          <div class="col-lg-6 col-md-6 col-sm-6 col-xs-6">
             <a href="//www.generalplastics.com" target="_blank">
               <img src="./img/logos/gold/generalplastics.jpg">
             </a>
@@ -57,51 +57,49 @@
         <h1>Silver Partners</h1>
 
         <div class="row silver">
-          <div class="col-lg-3 col-md-3 col-sm-6 col-xs-6">
+          <div class="col-lg-3 col-md-3 col-sm-4 col-xs-6">
             <a href="//www.navistar.com" target="_blank">
               <img src="./img/logos/silver/navistar.jpg">
             </a>
           </div>
 
-          <div class="col-lg-3 col-md-3 col-sm-6 col-xs-6">
+          <div class="col-lg-3 col-md-3 col-sm-4 col-xs-6">
             <a href="//www.skf.com" target="_blank">
               <img src="./img/logos/silver/skf.jpg">
             </a>
           </div>
 
-          <div class="col-lg-3 col-md-3 col-sm-6 col-xs-6">
+          <div class="col-lg-3 col-md-3 col-sm-4 col-xs-6">
             <a href="//www.precast.com" target="_blank">
               <img src="./img/logos/silver/precisioncastparts.jpg">
             </a>
           </div>
 
-          <div class="col-lg-3 col-md-3 col-sm-6 col-xs-6">
+          <div class="col-lg-3 col-md-3 col-sm-4 col-xs-6">
             <a href="//www.bosch.com" target="_blank">
               <img src="./img/logos/silver/bosch.jpg">
             </a>
           </div>
-        </div>
 
-        <div class="row silver">
-          <div class="col-lg-3 col-md-3 col-sm-6 col-xs-6">
+          <div class="col-lg-3 col-md-3 col-sm-4 col-xs-6">
             <a href="//www.mechanical.illinois.edu" target="_blank">
               <img src="./img/logos/silver/mechse.jpg">
             </a>
           </div>
 
-          <div class="col-lg-3 col-md-3 col-sm-6 col-xs-6">
+          <div class="col-lg-3 col-md-3 col-sm-4 col-xs-6">
             <a href="//www.itw.com" target="_blank">
               <img src="./img/logos/silver/itw.jpg">
             </a>
           </div>
 
-          <div class="col-lg-3 col-md-3 col-sm-6 col-xs-6">
+          <div class="col-lg-3 col-md-3 col-sm-4 col-md-offset-0 col-sm-offset-2 col-xs-6">
             <a href="//www.nscorp.com" target="_blank">
               <img src="./img/logos/silver/norfolksouthern.jpg">
             </a>
           </div>
 
-          <div class="col-lg-3 col-md-3 col-sm-6 col-xs-6">
+          <div class="col-lg-3 col-md-3 col-sm-4 col-xs-6">
             <a href="//www.toray.us" target="_blank">
               <img src="./img/logos/silver/toray.jpg">
             </a>
@@ -113,101 +111,97 @@
         <h1>Bronze Partners</h1>
 
         <div class="row bronze">
-          <div class="col-lg-2 col-md-2 col-sm-6 col-xs-6">
+          <div class="col-lg-2 col-md-2 col-sm-3 col-xs-6">
             <a href="//www.textreme.com" target="_blank">
               <img src="./img/logos/bronze/textreme.jpg">
             </a>
           </div>
 
-          <div class="col-lg-2 col-md-2 col-sm-6 col-xs-6">
+          <div class="col-lg-2 col-md-2 col-sm-3 col-xs-6">
             <a href="//www.imcousa.com" target="_blank">
               <img src="./img/logos/bronze/imcocarbidetools.jpg">
             </a>
           </div>
 
-          <div class="col-lg-2 col-md-2 col-sm-6 col-xs-6">
+          <div class="col-lg-2 col-md-2 col-sm-3 col-xs-6">
             <a href="//www.ford.com" target="_blank">
               <img src="./img/logos/bronze/ford.jpg">
             </a>
           </div>
 
-          <div class="col-lg-2 col-md-2 col-sm-6 col-xs-6">
+          <div class="col-lg-2 col-md-2 col-sm-3 col-xs-6">
             <a href="//www.bravotrailers.com" target="_blank">
               <img src="./img/logos/bronze/bravo.jpg">
             </a>
           </div>
 
-          <div class="col-lg-2 col-md-2 col-sm-6 col-xs-6">
+          <div class="col-lg-2 col-md-2 col-sm-3 col-xs-6">
             <a href="//www.cat.com" target="_blank">
               <img src="./img/logos/bronze/caterpillar.jpg">
             </a>
           </div>
 
-          <div class="col-lg-2 col-md-2 col-sm-6 col-xs-6">
+          <div class="col-lg-2 col-md-2 col-sm-3 col-xs-6">
             <a href="//www.kwalyti.com" target="_blank">
               <img src="./img/logos/bronze/kwalyti.jpg">
             </a>
           </div>
-        </div>
 
-        <div class="row bronze">
-          <div class="col-lg-2 col-md-2 col-sm-6 col-xs-6">
+          <div class="col-lg-2 col-md-2 col-sm-3 col-xs-6">
             <a href="//www.airtechonline.com/" target="_blank">
               <img src="./img/logos/bronze/airtech.jpg">
             </a>
           </div>
 
-          <div class="col-lg-2 col-md-2 col-sm-6 col-xs-6">
+          <div class="col-lg-2 col-md-2 col-sm-3 col-xs-6">
             <a href="//www.cummins.com" target="_blank">
               <img src="./img/logos/bronze/cummins.jpg">
             </a>
           </div>
 
-          <div class="col-lg-2 col-md-2 col-sm-6 col-xs-6">
+          <div class="col-lg-2 col-md-2 col-sm-3 col-xs-6">
             <a href="//www.bmrs.net" target="_blank">
               <img src="./img/logos/bronze/bmrs.jpg">
             </a>
           </div>
 
-          <div class="col-lg-2 col-md-2 col-sm-6 col-xs-6">
+          <div class="col-lg-2 col-md-2 col-sm-3 col-xs-6">
             <a href="//www.bellracing.com" target="_blank">
               <img src="./img/logos/bronze/bellracing.jpg">
             </a>
           </div>
 
-          <div class="col-lg-2 col-md-2 col-sm-6 col-xs-6">
+          <div class="col-lg-2 col-md-2 col-sm-3 col-xs-6">
             <a href="//www.boschautoparts.com/Pages/BoschMotorsports.aspx" target="_blank">
               <img src="./img/logos/bronze/boschmotorsport.jpg">
             </a>
           </div>
 
-          <div class="col-lg-2 col-md-2 col-sm-6 col-xs-6">
+          <div class="col-lg-2 col-md-2 col-sm-3 col-xs-6">
             <a href="//www.bennettmetal.com" target="_blank">
               <img src="./img/logos/bronze/bennettmetalproducts.jpg">
             </a>
           </div>
-        </div>
 
-        <div class="row bronze">
-          <div class="col-lg-2 col-md-2 col-sm-6 col-xs-6 col-md-offset-2 col-lg-offset-2">
+          <div class="col-lg-2 col-md-2 col-sm-3 col-xs-6 col-md-offset-2 col-lg-offset-2">
             <a href="//www.isthmuseng.com" target="_blank">
               <img src="./img/logos/bronze/isthmus.jpg">
             </a>
           </div>
 
-          <div class="col-lg-2 col-md-2 col-sm-6 col-xs-6">
+          <div class="col-lg-2 col-md-2 col-sm-3 col-xs-6">
             <a href="//www.plascore.com" target="_blank">
               <img src="./img/logos/bronze/plascore.jpg">
             </a>
           </div>
 
-          <div class="col-lg-2 col-md-2 col-sm-6 col-xs-6">
+          <div class="col-lg-2 col-md-2 col-sm-3 col-xs-6">
             <a href="//www.slb.com" target="_blank">
               <img src="./img/logos/bronze/schlumberger.jpg">
             </a>
           </div>
 
-          <div class="col-lg-2 col-md-2 col-sm-6 col-xs-6">
+          <div class="col-lg-2 col-md-2 col-sm-3 col-xs-6">
             <a href="//www.spacex.com" target="_blank">
               <img src="./img/logos/bronze/spacex.jpg">
             </a>
@@ -216,84 +210,81 @@
 
         <hr>
 
-        <!-- <h1>Honorary Partners</h1>
+        <h1>Honorary Partners</h1>
 
         <div class="row honorary">
-          <div class="col-lg-2 col-md-2 col-sm-6 col-xs-6">
+          <div class="col-lg-2 col-md-2 col-sm-4 col-xs-4">
             <a href="//www.reeseprod.com" target="_blank">
               <img src="http://placehold.it/300x150">
             </a>
           </div>
 
-          <div class="col-lg-2 col-md-2 col-sm-6 col-xs-6">
+          <div class="col-lg-2 col-md-2 col-sm-4 col-xs-4">
             <a href="//www.amsoil.com" target="_blank">
               <img src="http://placehold.it/300x150">
             </a>
           </div>
 
-          <div class="col-lg-2 col-md-2 col-sm-6 col-xs-6">
+          <div class="col-lg-2 col-md-2 col-sm-4 col-xs-4">
             <a href="//www.4pcb.com" target="_blank">
               <img src="http://placehold.it/300x150">
             </a>
           </div>
 
-          <div class="col-lg-2 col-md-2 col-sm-6 col-xs-6">
+          <div class="col-lg-2 col-md-2 col-sm-4 col-xs-4">
             <a href="//www.drpeppersnapplegroup.com" target="_blank">
               <img src="http://placehold.it/300x150">
             </a>
           </div>
 
-          <div class="col-lg-2 col-md-2 col-sm-6 col-xs-6">
+          <div class="col-lg-2 col-md-2 col-sm-4 col-xs-4">
             <a href="//www.wicksaircraft.com" target="_blank">
               <img src="http://placehold.it/300x150">
             </a>
           </div>
 
-          <div class="col-lg-2 col-md-2 col-sm-6 col-xs-6">
+          <div class="col-lg-2 col-md-2 col-sm-4 col-xs-4">
             <a href="//www.cmpanodizing.com/aboutCMPANODIZING.htm" target="_blank">
               <img src="http://placehold.it/300x150">
             </a>
           </div>
-        </div>
 
-        <div class="row honorary">
-          <div class="col-lg-2 col-md-2 col-sm-6 col-xs-6">
+          <div class="col-lg-2 col-md-2 col-sm-4 col-xs-4">
             <a href="//www.ptc.com" target="_blank">
               <img src="http://placehold.it/300x150">
             </a>
           </div>
 
-          <div class="col-lg-2 col-md-2 col-sm-6 col-xs-6">
+          <div class="col-lg-2 col-md-2 col-sm-4 col-xs-4">
             <a href="//www.mscsoftware.com" target="_blank">
               <img src="http://placehold.it/300x150">
             </a>
           </div>
 
-          <div class="col-lg-2 col-md-2 col-sm-6 col-xs-6">
+          <div class="col-lg-2 col-md-2 col-sm-4 col-xs-4">
             <a href="//www.cadsoftusa.com" target="_blank">
               <img src="http://placehold.it/300x150">
             </a>
           </div>
 
-          <div class="col-lg-2 col-md-2 col-sm-6 col-xs-6">
+          <div class="col-lg-2 col-md-2 col-sm-4 col-xs-4">
             <a href="//www.calspan.com" target="_blank">
               <img src="http://placehold.it/300x150">
             </a>
           </div>
 
-          <div class="col-lg-2 col-md-2 col-sm-6 col-xs-6">
+          <div class="col-lg-2 col-md-2 col-sm-4 col-xs-4">
             <a href="//www.wolfram.com" target="_blank">
               <img src="http://placehold.it/300x150">
             </a>
           </div>
 
-          <div class="col-lg-2 col-md-2 col-sm-6 col-xs-6">
+          <div class="col-lg-2 col-md-2 col-sm-4 col-xs-4">
             <a href="//www.schrothracing.com" target="_blank">
               <img src="http://placehold.it/300x150">
             </a>
           </div>
         </div>
-        -->
       </div>
     </div>
 
