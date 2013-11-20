fa9a4b702d4078b90c950c48816406a64f6af363
Andrew Mass <amass1212@gmail.com>
2013-11-20 16:47:34 -0600

    Adds small styling updates for join page.

--- php/join.php
+++ php/join.php
@@ -24,18 +24,24 @@
         </div>
 
         <div class="col-xs-12 col-md-10">
-          <div class="panel panel-info">
-            <div class="panel-body">
-              1: Join the Google Group
+          <div class="panel panel-default">
+            <div class="panel-heading">
+              <strong>1 &minus; Join the Google Group</strong>
             </div>
 
-            <div class="panel-footer">
-              We use Google Groups for all our communication. It is very important that you sign up and use an email you check often. Please join the main group and any subsystems that you are interested in!
+            <div class="panel-body">
+              <p>We use Google Groups for all our communication. It is very important that you sign up and use an email you check often. Please join the main group and any subsystems that you are interested in!</p>
 
               <ul>
-                <li><a target="_blank" href="//groups.google.com/group/illinimotorsports">Illini Motorsports Main Group</a></li>
+                <li>
+                  <a target="_blank" href="//groups.google.com/group/illinimotorsports">
+                    Illini Motorsports Main Group
+                  </a>
+                </li>
 
-                <li>Subsystems:
+                <li>
+                  Subsystems:
+
                   <ul>
                     <li><a target="_blank" href="//groups.google.com/group/illinimotorsportsengine">Engine</a></li>
 
@@ -50,23 +56,28 @@
                     <li><a target="_blank" href="//groups.google.com/group/illinimotorsportssuspension">Suspension</a></li>
                   </ul>
                 </li>
+              </ul>
             </div>
           </div>
 
-          <div class="panel panel-info">
-            <div class="panel-body">
-              2: Send us your contact information
+          <div class="panel panel-default">
+            <div class="panel-heading">
+              <strong>2 &minus; Send us your contact information</strong>
             </div>
 
-            <div class="panel-footer">That's as simple as going <a href="./contact.php">here</a>!</div>
+            <div class="panel-body">
+              <p>That's as simple as going <a href="./contact.php">here</a>!</p>
+            </div>
           </div>
 
-          <div class="panel panel-info">
-            <div class="panel-body">
-              3: Attend the meetings
+          <div class="panel panel-default">
+            <div class="panel-heading">
+              <strong>3 &minus; Attend the meetings</strong>
             </div>
 
-            <div class="panel-footer">We currently have a general meeting every Tuesday at 7:00pm. It is located in either the Engineering Student Project Laboratory or the Deere Pavilion (2005) at the Mechanical Engineering Laboratory. Please check the Google Group for updated meeting information.
+            <div class="panel-body">
+              <p>We currently have a general meeting every Tuesday at 7:00pm. It is located in either the Engineering Student Project Laboratory or the Deere Pavilion (2005) at the Mechanical Engineering Laboratory. Please check the Google Group for updated meeting information.</p>
+
               <iframe style="padding-top:15px" width="100%" height="400px" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://mapsengine.google.com/map/u/0/embed?mid=z9p9_kmSJe0U.kQs8bfI-t584"></iframe>
 
               <br />
