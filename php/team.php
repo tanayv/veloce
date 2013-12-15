6284cd7d32421be329a2e56c3baccf30b94ff436
Andrew Mass <amass1212@gmail.com>
2013-12-15 15:48:02 -0600

    Adds photos to repository and numerous updates for team page.

--- php/team.php
+++ php/team.php
@@ -15,7 +15,7 @@
     <?php include './header.php'; ?>
 
     <div class="content container">
-      <h1>2013-2014 Team</h1>
+      <h1>2013-2014 Team Leaders</h1>
 
       <div class="row">
         <div class="col-md-4">
@@ -26,7 +26,7 @@
 
             <div class="panel-body">
               <div class="profile-image">
-                <img src="//scontent-b-ord.xx.fbcdn.net/hphotos-frc3/268832_10151494440019753_1737865875_n.jpg">
+                <img src="./img/team/photos/bastanipour.jpg">
               </div>
 
               <table class="table table-hover">
@@ -38,7 +38,7 @@
 
                   <tr>
                     <td>Year</td>
-                    <td></td>
+                    <td>Junior</td>
                   </tr>
 
                   <tr>
@@ -48,13 +48,18 @@
 
                   <tr>
                     <td>Contact Email</td>
-                    <td><a href="mailto:"></a></td>
+                    <td><a href="mailto:bastani2@illinois.edu">bastani2@illinois.edu</a></td>
+                  </tr>
+
+                  <tr>
+                    <td>Phone Number</td>
+                    <td><a href="tel:18479178420">1-847-917-8420</a></td>
                   </tr>
                 </tbody>
               </table>
 
               <div class="well">
-                <p></p>
+                <p>Major: Mechanical Engineering<br>2011-2012: Engine/Electronics<br>2012-2013: Electronics Team Leader<br>2013-2014 Team Captain</p>
               </div>
             </div>
           </div>
@@ -68,7 +73,7 @@
 
             <div class="panel-body">
               <div class="profile-image">
-                <img src="//scontent-a-ord.xx.fbcdn.net/hphotos-ash2/999324_604859546213517_77371994_n.jpg">
+                <img src="./img/team/photos/miller.jpg">
               </div>
 
               <table class="table table-hover">
@@ -110,7 +115,7 @@
 
             <div class="panel-body">
               <div class="profile-image">
-                <img src="//my.engr.illinois.edu/directory/viewrolephoto.aspx?p=33720&s=100">
+                <img src="./img/team/photos/philpott.jpg">
               </div>
 
               <table class="table table-hover">
@@ -121,24 +126,14 @@
                   </thead>
 
                   <tr>
-                    <td>Year</td>
-                    <td></td>
-                  </tr>
-
-                  <tr>
-                    <td>Hometown</td>
-                    <td></td>
-                  </tr>
-
-                  <tr>
                     <td>Contact Email</td>
-                    <td><a href="mailto:"></a></td>
+                    <td><a href="mailto:mphilpot@illinois.edu">mphilpot@illinois.edu</a></td>
                   </tr>
                 </tbody>
               </table>
 
               <div class="well">
-                <p></p>
+                <p>Professor Philpott is the team's faculty advisor. He contributes years of experience to the team and helps to make important decisions. His research focuses on Nanomechanics and Nanomanufacturing.</p>
               </div>
             </div>
           </div>
@@ -154,7 +149,7 @@
 
             <div class="panel-body">
               <div class="profile-image">
-                <img src="//scontent-b-ord.xx.fbcdn.net/hphotos-frc3/525347_10151369219565773_2016179601_n.jpg">
+                <img src="./img/team/photos/kuhajda.jpg">
               </div>
 
               <table class="table table-hover">
@@ -196,7 +191,7 @@
 
             <div class="panel-body">
               <div class="profile-image">
-                <img src="//scontent-b-ord.xx.fbcdn.net/hphotos-ash4/1234435_10202269070073009_1630732599_n.jpg">
+                <img src="./img/team/photos/schwieters.jpg">
               </div>
 
               <table class="table table-hover">
@@ -208,23 +203,23 @@
 
                   <tr>
                     <td>Year</td>
-                    <td></td>
+                    <td>Junior</td>
                   </tr>
 
                   <tr>
                     <td>Hometown</td>
-                    <td></td>
+                    <td>Dellwood, Minnesota</td>
                   </tr>
 
                   <tr>
                     <td>Contact Email</td>
-                    <td><a href="mailto:"></a></td>
+                    <td><a href="mailto:schwiet2@illinois.edu">schwiet2@illinois.edu</a></td>
                   </tr>
                 </tbody>
               </table>
 
               <div class="well">
-                <p></p>
+                <p>As the leader of the Electronics Team, George takes a large interest in PCB design and embedded systems. Outside of Formula SAE, George enjoys maintaining and modifying cars, drag racing, and autocrossing. In his free time George also takes a large interst in playing guitar, snowboarding, and longboarding.</p>
               </div>
             </div>
           </div>
@@ -238,7 +233,7 @@
 
             <div class="panel-body">
               <div class="profile-image">
-                <img src="//scontent-a-ord.xx.fbcdn.net/hphotos-prn2/970970_10151777877940799_181250220_n.jpg">
+                <img src="./img/team/photos/harris.jpg">
               </div>
 
               <table class="table table-hover">
@@ -273,8 +268,6 @@
         </div>
       </div>
 
-      <hr>
-
       <div class="row">
         <div class="col-md-4">
           <div class="panel panel-default">
@@ -284,7 +277,7 @@
 
             <div class="panel-body">
               <div class="profile-image">
-                <img src="//scontent-a-ord.xx.fbcdn.net/hphotos-ash2/31443_10201784035506876_1086326347_n.jpg?lvh=1">
+                <img src="./img/team/photos/dempsey.jpg">
               </div>
 
               <table class="table table-hover">
@@ -326,7 +319,97 @@
 
             <div class="panel-body">
               <div class="profile-image">
-                <img src="//scontent-a-ord.xx.fbcdn.net/hphotos-prn2/960041_10201223077715925_1417002467_n.jpg">
+                <img src="./img/team/photos/sullivan.jpg">
+              </div>
+
+              <table class="table table-hover">
+                <tbody>
+                  <thead>
+                    <th>Biography</th>
+                    <th>
+                  </thead>
+
+                  <tr>
+                    <td>Year</td>
+                    <td>Senior</td>
+                  </tr>
+
+                  <tr>
+                    <td>Hometown</td>
+                    <td>Barrington, Illinois</td>
+                  </tr>
+
+                  <tr>
+                    <td>Contact Email</td>
+                    <td><a href="mailto:jsullvn2@illinois.edu">jsullvn2@illinois.edu</a></td>
+                  </tr>
+
+                  <tr>
+                    <td>Phone Number</td>
+                    <td><a href="tel:12245878179">1-224-587-8179</a>
+                </tbody>
+              </table>
+
+              <div class="well">
+                <p>Jon Sullivan is a senior at UIUC who is majoring in Mechanical Engineering. Past car fabrication and engine work eventually led to interest in FSAE. After working on the Drivetrain Team he was asked to lead it and thankfully has had no free time since. In the future he hopes to either work in the automotive industry or start his own company.</p>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div class="col-md-4">
+          <div class="panel panel-default">
+            <div class="panel-heading">
+              <strong>David Caruthers</strong> &minus; Aerodynamics Wizard
+            </div>
+
+            <div class="panel-body">
+              <div class="profile-image">
+                <img src="./img/team/photos/caruthers.jpg">
+              </div>
+
+              <table class="table table-hover">
+                <tbody>
+                  <thead>
+                    <th>Biography</th>
+                    <th>
+                  </thead>
+
+                  <tr>
+                    <td>Year</td>
+                    <td></td>
+                  </tr>
+
+                  <tr>
+                    <td>Hometown</td>
+                    <td></td>
+                  </tr>
+
+                  <tr>
+                    <td>Contact Email</td>
+                    <td><a href="mailto:"></a></td>
+                  </tr>
+                </tbody>
+              </table>
+
+              <div class="well">
+                <p></p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div class="row">
+        <div class="col-md-4">
+          <div class="panel panel-default">
+            <div class="panel-heading">
+              <strong>Adrian Liro</strong> &minus; Social Chair / Recruiting
+            </div>
+
+            <div class="panel-body">
+              <div class="profile-image">
+                <img src="./img/team/photos/liro.jpg">
               </div>
 
               <table class="table table-hover">
@@ -363,12 +446,12 @@
         <div class="col-md-4">
           <div class="panel panel-default">
             <div class="panel-heading">
-              <strong>David Caruthers</strong> &minus; Aerodynamics Wizard
+              <strong>Ed Husak</strong> &minus; Vice President / Chassis Member
             </div>
 
             <div class="panel-body">
               <div class="profile-image">
-                <img src="//scontent-a-ord.xx.fbcdn.net/hphotos-ash3/560522_3397226423087_1460034409_n.jpg">
+                <img src="./img/team/photos/husak.jpg">
               </div>
 
               <table class="table table-hover">
@@ -380,158 +463,70 @@
 
                   <tr>
                     <td>Year</td>
-                    <td></td>
+                    <td>Junior</td>
                   </tr>
 
                   <tr>
                     <td>Hometown</td>
-                    <td></td>
+                    <td>Lake in the Hills, Illinois</td>
                   </tr>
 
                   <tr>
                     <td>Contact Email</td>
-                    <td><a href="mailto:"></a></td>
+                    <td><a href="mailto:ehusak2@illinois.edu">ehusak2@illinois.edu</a></td>
                   </tr>
                 </tbody>
               </table>
 
               <div class="well">
-                <p></p>
+                <p>Ed is a junior studying General Engineering with a concentration in Automotive Technology. As Vice President, Ed handles many organizational tasks within Illini Motorsports such as travel, logistics, budget analysis, and university and sponsor relations. Ed has loved cars and been a gearhead his whole life. Ed hopes to work in the automotive industry upon graduation.</p>
               </div>
             </div>
           </div>
         </div>
 
-        <hr>
-
-        <div class="row">
-          <div class="col-md-4">
-            <div class="panel panel-default">
-              <div class="panel-heading">
-                <strong>Adrian Liro</strong> &minus; Social Chair / Recruiting
-              </div>
-
-              <div class="panel-body">
-                <div class="profile-image">
-                  <img src="//scontent-a-ord.xx.fbcdn.net/hphotos-ash3/1441380_591915584177416_61658851_n.jpg">
-                </div>
-
-                <table class="table table-hover">
-                  <tbody>
-                    <thead>
-                      <th>Biography</th>
-                      <th>
-                    </thead>
-
-                    <tr>
-                      <td>Year</td>
-                      <td></td>
-                    </tr>
-
-                    <tr>
-                      <td>Hometown</td>
-                      <td></td>
-                    </tr>
-
-                    <tr>
-                      <td>Contact Email</td>
-                      <td><a href="mailto:"></a></td>
-                    </tr>
-                  </tbody>
-                </table>
-
-                <div class="well">
-                  <p></p>
-                </div>
-              </div>
+        <div class="col-md-4">
+          <div class="panel panel-default">
+            <div class="panel-heading">
+              <strong>Alex Allmandinger</strong> &minus; Aerodynamics Member
             </div>
-          </div>
 
-          <div class="col-md-4">
-            <div class="panel panel-default">
-              <div class="panel-heading">
-                <strong>Ed Husak</strong> &minus; Vice President / Chassis Team Member
+            <div class="panel-body">
+              <div class="profile-image">
+                <img src="./img/team/photos/allmandinger.jpg">
               </div>
 
-              <div class="panel-body">
-                <div class="profile-image">
-                  <img src="//scontent-b-ord.xx.fbcdn.net/hphotos-ash4/1459942_10202507667279087_95686281_n.jpg">
-                </div>
+              <table class="table table-hover">
+                <tbody>
+                  <thead>
+                    <th>Biography</th>
+                    <th>
+                  </thead>
 
-                <table class="table table-hover">
-                  <tbody>
-                    <thead>
-                      <th>Biography</th>
-                      <th>
-                    </thead>
+                  <tr>
+                    <td>Year</td>
+                    <td>Sophomore</td>
+                  </tr>
 
-                    <tr>
-                      <td>Year</td>
-                      <td>Junior</td>
-                    </tr>
+                  <tr>
+                    <td>Hometown</td>
+                    <td>Crystal Lake, Illinois</td>
+                  </tr>
 
-                    <tr>
-                      <td>Hometown</td>
-                      <td>Lake in the Hills, Illinois</td>
-                    </tr>
+                  <tr>
+                    <td>Resume</td>
+                    <td><a target="_blank" href="./img/team/resume/alex-allmandinger.pdf">View Resume</a></td>
+                  </tr>
 
-                    <tr>
-                      <td>Contact Email</td>
-                      <td><a href="mailto:ehusak2@illinois.edu">ehusak2@illinois.edu</a></td>
-                    </tr>
-                  </tbody>
-                </table>
+                  <tr>
+                    <td>Contact Email</td>
+                    <td><a href="mailto:allmand2@illinois.edu">allmand2@illinois.edu</a></td>
+                  </tr>
+                </tbody>
+              </table>
 
-                <div class="well">
-                  <p>Ed is a junior studying General Engineering with a concentration in Automotive Technology. As Vice President, Ed handles many organizational tasks within Illini Motorsports such as travel, logistics, budget analysis, and university and sponsor relations. Ed has loved cars and been a gearhead his whole life. Ed hopes to work in the automotive industry upon graduation.</p>
-                </div>
-              </div>
-            </div>
-          </div>
-
-          <div class="col-md-4">
-            <div class="panel panel-default">
-              <div class="panel-heading">
-                <strong>Alex Allmandinger</strong> &minus; Aerodynamics Team Member
-              </div>
-
-              <div class="panel-body">
-                <div class="profile-image">
-                  <img src="//fbcdn-profile-a.akamaihd.net/hprofile-ak-ash2/c44.44.552.552/s160x160/555209_3611252413192_148360088_n.jpg">
-                </div>
-
-                <table class="table table-hover">
-                  <tbody>
-                    <thead>
-                      <th>Biography</th>
-                      <th>
-                    </thead>
-
-                    <tr>
-                      <td>Year</td>
-                      <td>Sophomore</td>
-                    </tr>
-
-                    <tr>
-                      <td>Hometown</td>
-                      <td>Crystal Lake, Illinois</td>
-                    </tr>
-
-                    <tr>
-                      <td>Resume</td>
-                      <td><a target="_blank" href="./img/team/resume/alex-allmandinger.pdf">View Resume</a></td>
-                    </tr>
-
-                    <tr>
-                      <td>Contact Email</td>
-                      <td><a href="mailto:allmand2@illinois.edu">allmand2@illinois.edu</a></td>
-                    </tr>
-                  </tbody>
-                </table>
-
-                <div class="well">
-                  <p>Alex is a sophomore in Mechanical Engineering, with experience in construction and mining industries, as well as the automotive industry. He is currently leading the development of the Drag Reduction System.</p>
-                </div>
+              <div class="well">
+                <p>Alex is a sophomore in Mechanical Engineering, with experience in construction and mining industries, as well as the automotive industry. He is currently leading the development of the Drag Reduction System.</p>
               </div>
             </div>
           </div>
