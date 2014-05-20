6e2447d33d96ceda81c00a8749fd9f32409f9da5
Andrew Mass <amass1212@gmail.com>
2014-05-20 11:26:52 -0500

    Add 2014 car page.

--- php/cars.php
+++ php/cars.php
@@ -17,6 +17,165 @@
     <div class="content container">
       <div class="panel-group" id="accordion">
         <div class="panel panel-default">
+          <div class="panel-heading accordion-toggle" data-toggle="collapse" data-parent="#accordion" href="#collapse-2014">
+            <h4 class="panel-title">
+              <a class="accordion-toggle" data-toggle="collapse" data-parent="#accordion" href="#collapse-2014">
+                2013-2014
+              </a>
+            </h4>
+          </div>
+
+          <div id="collapse-2014" class="panel-collapse collapse in">
+            <div class="panel-body">
+              <div class="row">
+                <div class="col-md-4">
+                  <h2>2013-2014</h2>
+
+                  <p>Illini Motorsports 2014 competition entry has been designed using points analysis coupled lap simulation and a renewed focus on reliability. Key design changes include a new aerodynamics package featuring custom airfoils, a reduction in wheel base and an introduction of a dry sump lubrication system. We would like to give a special thanks to our friends, families, and sponsors for their continued support.</p>
+
+                  <table class="table table-hover">
+                    <thead>
+                      <th>Results</th>
+                      <th>
+                    </thead>
+
+                    <tbody>
+                      <tr>
+                        <td>FSAE Michigan 2014</td>
+                        <td>17th Overall</td>
+                      </tr>
+
+                      <tr>
+                        <td>FSAE Lincoln 2014</td>
+                        <td>To Be Determined</td>
+                      </tr>
+                    </tbody>
+                  </table>
+                </div>
+
+                <div class="col-md-8">
+                  <img src="./img/car_2014.jpg">
+                </div>
+              </div>
+
+              <div class="row">
+                <div class="col-md-6">
+                  <table class="table table-hover">
+                    <thead>
+                      <th>Data Points</th>
+                      <th>
+                    </thead>
+
+                    <tbody>
+                      <tr>
+                        <td>Brake</td>
+                        <td>AP Racing 4-piston front, 2-piston rear</td>
+                      </tr>
+
+                      <tr>
+                        <td>BSCD</td>
+                        <td>67mm bore / 42.5mm stroke / 599cc</td>
+                      </tr>
+
+                      <tr>
+                        <td>Cooling</td>
+                        <td>Vertically mounted single core double pass radiator, 944 cfm fan</td>
+                      </tr>
+
+                      <tr>
+                        <td>Drive</td>
+                        <td>RWD</td>
+                      </tr>
+
+                      <tr>
+                        <td>Electronics</td>
+                        <td>Motec M400 ECU and student designed data aquisition</td>
+                      </tr>
+
+                      <tr>
+                        <td>Engine</td>
+                        <td>Honda CBR600 F4i</td>
+                      </tr>
+
+                      <tr>
+                        <td>FR/RR Track</td>
+                        <td>49in front, 49in rear</td>
+                      </tr>
+
+                      <tr>
+                        <td>Frame</td>
+                        <td>4130 Steel space frame</td>
+                      </tr>
+
+                      <tr>
+                        <td>Fuel System</td>
+                        <td>Stock Honda</td>
+                      </tr>
+                    </tbody>
+                  </table>
+                </div>
+
+                <div class="col-md-6">
+                  <table class="table table-hover">
+                    <thead>
+                      <th>Data Points</th>
+                      <th>
+                    </thead>
+
+                    <tbody>
+                      <tr>
+                        <td>Fuel Type</td>
+                        <td>93 Octane</td>
+                      </tr>
+
+                      <tr>
+                        <td>MPD</td>
+                        <td>10500 rpm</td>
+                      </tr>
+
+                      <tr>
+                        <td>MPT</td>
+                        <td>9000 rpm</td>
+                      </tr>
+
+                      <tr>
+                        <td>OLWH</td>
+                        <td>L: 3249mm / W: 1468mm / H: 1124mm</td>
+                      </tr>
+
+                      <tr>
+                        <td>Suspension</td>
+                        <td>Double unequal length A-arms, Ohlins TTX25 dampers</td>
+                      </tr>
+
+                      <tr>
+                        <td>Tire</td>
+                        <td>20.5x7x13 Hoosier R25B</td>
+                      </tr>
+
+                      <tr>
+                        <td>Unique</td>
+                        <td>Custom airfoils and custom CFRP steering wheel</td>
+                      </tr>
+
+                      <tr>
+                        <td>Weight</td>
+                        <td>640 lbs with 150lb driver</td>
+                      </tr>
+
+                      <tr>
+                        <td>Wheelbase</td>
+                        <td>61in</td>
+                      </tr>
+                    </tbody>
+                  </table>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div class="panel panel-default">
           <div class="panel-heading accordion-toggle" data-toggle="collapse" data-parent="#accordion" href="#collapse-2013">
             <h4 class="panel-title">
               <a class="accordion-toggle" data-toggle="collapse" data-parent="#accordion" href="#collapse-2013">
@@ -25,7 +184,7 @@
             </h4>
           </div>
 
-          <div id="collapse-2013" class="panel-collapse collapse in">
+          <div id="collapse-2013" class="panel-collapse collapse">
             <div class="panel-body">
               <div class="row">
                 <div class="col-md-4">
