b517e9eb90c684cc9370621bd21e4f06b3fc1647
Andrew Mass <amass1212@gmail.com>
2014-10-28 18:33:09 -0500

    Remove useless footer. Update join page.
    Remove useless template file.

--- php/footer.php
+++ php/footer.php
@@ -6,38 +6,4 @@
 ?>
 
 <?php include './topsponsors.php'; ?>
-
 <script type="text/javascript" src="http://emergency.webservices.illinois.edu/illinois.js"></script>
-<div class="footer">
-  <div class="container">
-    <div class="row">
-      <div class="col-xs-9" id="footer-left">
-        <p>
-          <a href="//www.youtube.com/user/IlliniMotorsports" target="_blank">
-            Youtube
-          </a>
-
-          |
-
-          <a href="//www.facebook.com/IlliniMotorsports" target="_blank">
-            Facebook
-          </a>
-
-          |
-
-          <a href="//www.twitter.com/IllinoisFSAE" target="_blank">
-            Twitter
-          </a>
-        </p>
-      </div>
-
-      <div class="col-xs-3" id="footer-right">
-        <p>
-          <a href="./contact.php">
-            Contact Us
-          </a>
-        </p>
-      </div>
-    </div>
-  </div>
-</div>
--- php/join.php
+++ php/join.php
@@ -32,13 +32,18 @@
             <div class="panel-body">
               <p>We use Google Groups for all our communication. It is very important that you sign up and use an email you check often. Please join the main group and any subsystems that you are interested in!</p>
 
-              <ul>
-                <li>
-                  <a target="_blank" href="//groups.google.com/group/illinimotorsports">
-                    Illini Motorsports Main Group
-                  </a>
-                </li>
+              <strong><p>Make sure you set the notification settings to send you an email for each new message on the group!</p></strong>
 
+              <strong><p>Also, you can't use your illinois.edu email address. You must use a personal email address.</p></strong>
+
+              <strong>
+              <a target="_blank" href="//groups.google.com/group/illinimotorsports">
+                <p>Illini Motorsports Main Group</p>
+              </a>
+              </strong>
+
+              <!--
+              <ul>
                 <li>
                   Subsystems:
 
@@ -57,6 +62,7 @@
                   </ul>
                 </li>
               </ul>
+              -->
             </div>
           </div>
 
@@ -76,11 +82,11 @@
             </div>
 
             <div class="panel-body">
-              <p>We currently have a general meeting every Tuesday at 7:00pm. It is located in either the Engineering Student Project Laboratory or the Deere Pavilion (2005) at the Mechanical Engineering Laboratory. Please check the Google Group for updated meeting information.</p>
+              <p>We currently have a general meeting every Tuesday at 7:00pm. It is located the Engineering Student Project Laboratory, 153 MEB, or the John Deere Pavilion (2005) at the Mechanical Engineering Laboratory. Please check the Google Group for updated meeting information.</p>
 
               <iframe style="padding-top:15px" width="100%" height="400px" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://mapsengine.google.com/map/u/0/embed?mid=z9p9_kmSJe0U.kQs8bfI-t584"></iframe>
 
-              <br />
+              <br/>
 
               <small>
                 <a target="_blank" href="https://mapsengine.google.com/map/u/0/embed?mid=z9p9_kmSJe0U.kQs8bfI-t584">View Larger Map</a>
