53da53c97211ead9c681c0d3398366f498c0648b
Andrew Mass <amass1212@gmail.com>
2015-04-14 18:57:57 -0500

    Fix broken donation link.

--- php/give.php
+++ php/give.php
@@ -5,8 +5,8 @@
   </head>
 
   <div class="alert alert-danger" style="margin: 0px; font-size:25px; text-align: center;">
-    Make sure to select <strong>"Formula Car"</strong> or the funds will not go to the team. Thank you for your generosity!
+    Make sure to select <strong>"Student Society Support -> SAE Formula Car Fund"</strong> or the funds will not go to the team. Thank you for your generosity!
   </div>
 
-  <iframe style="height: calc(100% - 36px); width:100%" src="http://web.mechse.illinois.edu/content/for/alumni/gifts/make_a_gift.php"></iframe>
+  <iframe style="height: calc(100% - 36px); width:100%" src="http://mechse.illinois.edu/donate/making-online-donation"></iframe>
 </html>
