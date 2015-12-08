58e2da09f7f23b7084ade15953c30c3350e726a7
Andrew Mass <amass1212@gmail.com>
2015-12-07 23:41:26 -0600

    Update some of the team leaders for 2015-2016.

--- php/team.php
+++ php/team.php
@@ -8,6 +8,440 @@
     <?php include './header.php'; ?>
 
     <div class="content container">
+      <h1>2015-2016 Team Leaders</h1>
+
+      <div class="row">
+        <div class="col-md-4">
+          <div class="panel panel-default">
+            <div class="panel-heading">
+              <strong>John Guidone</strong>
+            </div>
+
+            <div class="panel-body">
+              <div class="profile-image">
+                <img src="./img/team/photos/2016/guidone.jpg">
+              </div>
+
+              <table class="table table-hover">
+                <tbody>
+                  <thead>
+                    <th colspan="2">Team Captain &amp; Chassis Subsystem Leader</th>
+                  </thead>
+
+                  <tr>
+                    <td>Year</td>
+                    <td>Senior</td>
+                  </tr>
+
+                  <tr>
+                    <td>Hometown</td>
+                    <td>Chicago, Illinois</td>
+                  </tr>
+
+                  <tr>
+                    <td>NetID</td>
+                    <td><a href="./contact.php?chassis=y">guidone2</a></td>
+                  </tr>
+
+                </tbody>
+              </table>
+
+              <div class="well">
+                <p>John is a Senior in Mechanical Engineering and has the roll of Team Captain. John has also been involved with the Chassis and Integration Team ever since freshman year and is the current Chassis and Integration Team Leader. Guidone is planning on pursuing a Masters of Science in Mechanical Engineering and eventually becoming a race engineer in professional motorsports.</p>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div class="col-md-4">
+          <div class="panel panel-default">
+            <div class="panel-heading">
+              <strong>Keith Harris</strong>
+            </div>
+
+            <div class="panel-body">
+              <div class="profile-image">
+                <img src="./img/team/photos/harris.jpg">
+              </div>
+
+              <table class="table table-hover">
+                <tbody>
+                  <thead>
+                    <th colspan="2">Chief Engineer &amp; Main Driver</th>
+                  </thead>
+
+                  <tr>
+                    <td>Year</td>
+                    <td>Senior</td>
+                  </tr>
+
+                  <tr>
+                    <td>Hometown</td>
+                    <td>Champaign, Illinois</td>
+                  </tr>
+
+                  <tr>
+                    <td>NetID</td>
+                    <td><a href="./contact.php">kgharri2</a></td>
+                  </tr>
+                </tbody>
+              </table>
+
+              <div class="well">
+                <p></p>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div class="col-md-4">
+          <div class="panel panel-default">
+            <div class="panel-heading">
+              <strong>Professor Michael Philpott</strong>
+            </div>
+
+            <div class="panel-body">
+              <div class="profile-image">
+                <img src="./img/team/photos/philpott.jpg">
+              </div>
+
+              <table class="table table-hover">
+                <tbody>
+                  <thead>
+                    <th colspan="2">Faculty Advisor</th>
+                  </thead>
+
+                  <tr>
+                    <td>Office</td>
+                    <td>2007 Mechanical Engineering Lab</td>
+                  </tr>
+
+                  <tr>
+                    <td>Research</td>
+                    <td>Nanomechanics &amp; Nanomanufacturing</td>
+                  </tr>
+
+                  <tr>
+                    <td>NetID</td>
+                    <td>mphilpot</td>
+                  </tr>
+
+                </tbody>
+              </table>
+
+              <div class="well">
+                <p>As the faculty advisor, Professor Philpott oversees all of the Illini Motorsports teams, including Baja and Formula Electric. He also leads the one credit hour ME 199 that meets on Mondays at 4pm in DCL. </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div class="row">
+        <div class="col-md-4">
+          <div class="panel panel-default">
+            <div class="panel-heading">
+              <strong>Runshi (Rex) Gu</strong>
+            </div>
+
+            <div class="panel-body">
+              <div class="profile-image">
+                <img src="./img/team/photos/2016/gu.jpg">
+              </div>
+
+              <table class="table table-hover">
+                <tbody>
+                  <thead>
+                    <th colspan="2">President</th>
+                  </thead>
+
+                  <tr>
+                    <td>Year</td>
+                    <td>Junior</td>
+                  </tr>
+
+                  <tr>
+                    <td>Hometown</td>
+                    <td>Suzhou, China</td>
+                  </tr>
+
+                  <tr>
+                    <td>NetID</td>
+                    <td><a href="./contact.php">rgu2</a></td>
+                  </tr>
+
+                </tbody>
+              </table>
+
+              <div class="well">
+                <p>Rex is a Junior in Mechanical Engineering at UIUC. He deals with all external affairs to ensure the team has the proper resources it needs to complete the racecar. He is also on engine team and focusing on optimizing the new intake for the new race motor. When he is not working on the car, Rex enjoys playing basketball and napping.</p>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div class="col-md-4">
+          <div class="panel panel-default">
+            <div class="panel-heading">
+              <strong>Andrew Mass</strong>
+            </div>
+
+            <div class="panel-body">
+              <div class="profile-image">
+                <img src="./img/team/photos/2016/mass.jpg">
+              </div>
+
+              <table class="table table-hover">
+                <tbody>
+                  <thead>
+                    <th colspan="2">Electronics Subsystem Leader</th>
+                  </thead>
+
+                  <tr>
+                    <td>Year</td>
+                    <td>Junior</td>
+                  </tr>
+
+                  <tr>
+                    <td>Hometown</td>
+                    <td>Barrington, Illinois</td>
+                  </tr>
+
+                  <tr>
+                    <td>NetID</td>
+                    <td><a href="./contact.php?electronics=y">amass2</a></td>
+                  </tr>
+
+                </tbody>
+              </table>
+
+              <div class="well">
+                <p>Andrew is a junior in computer engineering at UIUC. He has been involved with the Electronics subsystem since freshman year. This year, he is in charge of every electron on the car. When he's not soldering, wiring, and coding, he likes to water-ski and play hockey.</p>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div class="col-md-4">
+          <div class="panel panel-default">
+            <div class="panel-heading">
+              <strong>Alex Gray</strong>
+            </div>
+
+            <div class="panel-body">
+              <div class="profile-image">
+                <img src="./img/team/photos/2016/gray.jpg">
+              </div>
+
+              <table class="table table-hover">
+                <tbody>
+                  <thead>
+                    <th colspan="2">Suspension Subsystem Leader</th>
+                  </thead>
+
+                  <tr>
+                    <td>Year</td>
+                    <td>Senior</td>
+                  </tr>
+
+                  <tr>
+                    <td>Hometown</td>
+                    <td>Algonquin, Illinois</td>
+                  </tr>
+
+                  <tr>
+                    <td>NetID</td>
+                    <td><a href="./contact.php?suspension=y">atgray2</a></td>
+                  </tr>
+
+                </tbody>
+              </table>
+
+              <div class="well">
+                <p>Alex is a senior in mechanical engineering. His interest in cars lead him to pursue engineering and FSAE. He transferred to the university as a junior and quickly worked his way up to a team leader position. This year he is implementing a highly adjustable suspension with a focus on tuning and adjustment accuracy. After school, Alex hopes to work in the automotive industry.</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div class="row">
+        <div class="col-md-4">
+          <div class="panel panel-default">
+            <div class="panel-heading">
+              <strong>Anthony Klepacki</strong>
+            </div>
+
+            <div class="panel-body">
+              <div class="profile-image">
+                <img src="./img/team/photos/2016/klepacki.jpg">
+              </div>
+
+              <table class="table table-hover">
+                <tbody>
+                  <thead>
+                    <th colspan="2">Composities Subsystem Leader</th>
+                  </thead>
+
+                  <tr>
+                    <td>Year</td>
+                    <td>Junior</td>
+                  </tr>
+
+                  <tr>
+                    <td>Hometown</td>
+                    <td>Chicago, Illinois</td>
+                  </tr>
+
+                  <tr>
+                    <td>NetID</td>
+                    <td><a href="./contact.php?composities=y">aklepa4</a></td>
+                  </tr>
+
+                </tbody>
+              </table>
+
+              <div class="well">
+                <p>Anthony is a junior studying aerospace engineering at the University of Illinois. He is the subsystem leader responsible for designing and manufacturing composites based components for the vehicle.  He would like to use the knowledge and skills he learns at school and on the team to pursue a career in the automotive industry and motorsports.</p>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div class="col-md-4">
+          <div class="panel panel-default">
+            <div class="panel-heading">
+              <strong>Shreyas Sudhakar</strong>
+            </div>
+
+            <div class="panel-body">
+              <div class="profile-image">
+                <img src="./img/team/photos/2016/sudhakar.jpg">
+              </div>
+
+              <table class="table table-hover">
+                <tbody>
+                  <thead>
+                    <th colspan="2">Engine Subsystem Leader</th>
+                  </thead>
+
+                  <tr>
+                    <td>Year</td>
+                    <td>Senior</td>
+                  </tr>
+
+                  <tr>
+                    <td>Hometown</td>
+                    <td>Canton, Ohio</td>
+                  </tr>
+
+                  <tr>
+                    <td>NetID</td>
+                    <td><a href="./contact.php?engine=y">sudhakr2</a></td>
+                  </tr>
+                </tbody>
+              </table>
+
+              <div class="well">
+              <p>Shreyas is a senior in Mechanical Engineering, and is leading the engine subsystem. He joined Illini Motorsports his freshman year after a lifelong love for cars, and has been on engine team ever since. This year, he is leading the team through its transition to a new engine platform, coordinating simulation, design, integration and calibrations.  </p>
+              </div>
+            </div>
+          </div>
+        </div>
+
+ <!--
+        <div class="col-md-4">
+          <div class="panel panel-default">
+            <div class="panel-heading">
+              <strong>Naveed Rahman</strong>
+            </div>
+
+            <div class="panel-body">
+              <div class="profile-image">
+                <img src="./img/team/photos/person.jpg">
+              </div>
+
+              <table class="table table-hover">
+                <tbody>
+                  <thead>
+                    <th colspan="2">Aerodynamics Subsytem Leader</th>
+                  </thead>
+
+                  <tr>
+                    <td>Year</td>
+                    <td>Year</td>
+                  </tr>
+
+                  <tr>
+                    <td>Hometown</td>
+                    <td>Town, State</td>
+                  </tr>
+
+                  <tr>
+                    <td>NetID</td>
+                    <td><a href="./contact.php">netid</a></td>
+                  </tr>
+
+                </tbody>
+              </table>
+
+              <div class="well">
+                <p></p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+ -->
+
+<!--
+      <div class="row">
+        <div class="col-md-4">
+          <div class="panel panel-default">
+            <div class="panel-heading">
+              <strong>Austin Kirchner</strong>
+            </div>
+
+            <div class="panel-body">
+              <div class="profile-image">
+                <img src="./img/team/photos/person.jpg">
+              </div>
+
+              <table class="table table-hover">
+                <tbody>
+                  <thead>
+                    <th colspan="2">Drivetrain Subsystem Leader</th>
+                  </thead>
+
+                  <tr>
+                    <td>Year</td>
+                    <td>Year</td>
+                  </tr>
+
+                  <tr>
+                    <td>Hometown</td>
+                    <td>Town, State</td>
+                  </tr>
+
+                  <tr>
+                    <td>NetID</td>
+                    <td><a href="./contact.php">netid</a></td>
+                  </tr>
+
+                </tbody>
+              </table>
+
+              <div class="well">
+                <p></p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+-->
+    </div>
+
+    <div class="content container">
       <h1>2014-2015 Team Leaders</h1>
 
       <div class="row">
