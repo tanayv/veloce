3af99091dbb18635a2fc85f32f2b59e92ba56eee
Andrew Mass <amass1212@gmail.com>
2016-11-11 05:23:49 -0600

    Add team page for 2016-2017.

--- php/team.php
+++ php/team.php
@@ -8,6 +8,512 @@
     <?php include './header.php'; ?>
 
     <div class="content container">
+      <h1>2016-2017 Team Leaders</h1>
+
+      <div class="row">
+        <div class="col-md-4">
+          <div class="panel panel-default">
+            <div class="panel-heading">
+              <strong>Anthony Klepacki</strong>
+            </div>
+
+            <div class="panel-body">
+              <div class="profile-image">
+                <img src="./img/team/photos/2017/klepacki.jpg">
+              </div>
+
+              <table class="table table-hover">
+                <tbody>
+                  <thead>
+                    <th colspan="2">Team Captain &amp; Composites Lead</th>
+                  </thead>
+
+                  <tr>
+                    <td>Year</td>
+                    <td>Senior</td>
+                  </tr>
+
+                  <tr>
+                    <td>Hometown</td>
+                    <td>Chicago, Illinois</td>
+                  </tr>
+
+                  <tr>
+                    <td>NetID</td>
+                    <td><a href="./contact.php?composites=y">aklepa4</a></td>
+                  </tr>
+
+                </tbody>
+              </table>
+
+              <div class="well">
+                <p>Anthony is a senior studying aerospace engineering at the University of Illinois. He is the team captain and subsystem leader responsible for designing and manufacturing composites based components for the vehicle. He would like to use the knowledge and skills he learns at school and on the team to pursue a career in the automotive industry and motorsports with a focus on advanced materials and manufacturing practices.</p>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div class="col-md-4">
+          <div class="panel panel-default">
+            <div class="panel-heading">
+              <strong>Rex Gu</strong>
+            </div>
+
+            <div class="panel-body">
+              <div class="profile-image">
+                <img src="./img/team/photos/2017/gu.jpg">
+              </div>
+
+              <table class="table table-hover">
+                <tbody>
+                  <thead>
+                    <th colspan="2">President</th>
+                  </thead>
+
+                  <tr>
+                    <td>Year</td>
+                    <td>Senior</td>
+                  </tr>
+
+                  <tr>
+                    <td>Hometown</td>
+                    <td>Suzhou, China</td>
+                  </tr>
+
+                  <tr>
+                    <td>NetID</td>
+                    <td><a href="./contact.php">rgu2</a></td>
+                  </tr>
+
+                </tbody>
+              </table>
+
+              <div class="well">
+                <p>Rex is a senior in mechanical engineering at UIUC. He deals with all external affairs to ensure the team has the proper resources it needs to complete the racecar. He is also on engine team and focusing on optimizing the new intake for the new race motor. When he is not working on the car, Rex enjoys playing basketball and napping. He hopes to pursue a future in automotive engineering and eventually start his own business in the automotive field.</p>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div class="col-md-4">
+          <div class="panel panel-default">
+            <div class="panel-heading">
+              <strong>Alex Allmandinger</strong>
+            </div>
+
+            <div class="panel-body">
+              <div class="profile-image">
+                <img src="./img/team/photos/2017/allmandinger.jpg">
+              </div>
+
+              <table class="table table-hover">
+                <tbody>
+                  <thead>
+                    <th colspan="2">Chief Engineer</th>
+                  </thead>
+
+                  <tr>
+                    <td>Year</td>
+                    <td>Senior</td>
+                  </tr>
+
+                  <tr>
+                    <td>Hometown</td>
+                    <td>Crystal Lake, Illinois</td>
+                  </tr>
+
+                  <tr>
+                    <td>NetID</td>
+                    <td><a href="./contact.php">allmand2</a></td>
+                  </tr>
+
+                </tbody>
+              </table>
+
+              <div class="well">
+                <p>Now a senior in mechanical engineering, Alex has spend a majority of his time on the team working in Aerodynamics and Composites, but now is in an expanded role as Chief Engineer. After taking a year away from school to work in F1 with the Infiniti Engineering Academy, he is excited to finish his studies and apply all of his newly learned skills to the Illini Motorsports team. Away from racing and engineering, he enjoys hanging out with his friends and eating Mexican food.</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div class="row">
+        <div class="col-md-4">
+          <div class="panel panel-default">
+            <div class="panel-heading">
+              <strong>Professor Michael Philpott</strong>
+            </div>
+
+            <div class="panel-body">
+              <div class="profile-image">
+                <img src="./img/team/photos/philpott.jpg">
+              </div>
+
+              <table class="table table-hover">
+                <tbody>
+                  <thead>
+                    <th colspan="2">Faculty Advisor</th>
+                  </thead>
+
+                  <tr>
+                    <td>Office</td>
+                    <td>2007 Mechanical Engineering Lab</td>
+                  </tr>
+
+                  <tr>
+                    <td>NetID</td>
+                    <td>mphilpot</td>
+                  </tr>
+
+                </tbody>
+              </table>
+
+              <div class="well">
+                <p>As the faculty advisor, Professor Philpott oversees all of the SAE teams, including Illini Motorsports, Baja, and Formula Electric. He also leads the one credit hour ME 199 that meets every Monday at 4pm.</p>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div class="col-md-4">
+          <div class="panel panel-default">
+            <div class="panel-heading">
+              <strong>Professor Bruce Flachsbart</strong>
+            </div>
+
+            <div class="panel-body">
+              <div class="profile-image">
+                <img src="./img/team/photos/flachsbart.jpg">
+              </div>
+
+              <table class="table table-hover">
+                <tbody>
+                  <thead>
+                    <th colspan="2">Faculty Advisor &amp; ESPL Director</th>
+                  </thead>
+
+                  <tr>
+                    <td>Office</td>
+                    <td>264 Mechanical Engineering Bldg</td>
+                  </tr>
+
+                  <tr>
+                    <td>NetID</td>
+                    <td>mems</td>
+                  </tr>
+
+                </tbody>
+              </table>
+
+              <div class="well">
+                <p>Professor Flachsbart acts as a faculty advisor and oversees the Engineering Student Project Laboratories where Illini Motorsports designs and builds the vehicle each year. Professor Flachsbart also helps the team with university affairs and acts as a liaison between the team and the College of Engineering.</p>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div class="col-md-4">
+          <div class="panel panel-default">
+            <div class="panel-heading">
+              <strong>Andrew Mass</strong>
+            </div>
+
+            <div class="panel-body">
+              <div class="profile-image">
+                <img src="./img/team/photos/2017/mass.jpg">
+              </div>
+
+              <table class="table table-hover">
+                <tbody>
+                  <thead>
+                    <th colspan="2">Electronics Subsystem Lead</th>
+                  </thead>
+
+                  <tr>
+                    <td>Year</td>
+                    <td>Senior</td>
+                  </tr>
+
+                  <tr>
+                    <td>Hometown</td>
+                    <td>Barrington, Illinois</td>
+                  </tr>
+
+                  <tr>
+                    <td>NetID</td>
+                    <td><a href="./contact.php?electronics=y">amass2</a></td>
+                  </tr>
+
+                </tbody>
+              </table>
+
+              <div class="well">
+                <p>Andrew is a senior in computer engineering at UIUC. He has been involved with the Electronics subsystem since freshman year. This year, he is in charge of every electron on the car. When he's not soldering, wiring, and coding, he likes to water-ski and play hockey.</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div class="row">
+        <div class="col-md-4">
+          <div class="panel panel-default">
+            <div class="panel-heading">
+              <strong>Ben Williams</strong>
+            </div>
+
+            <div class="panel-body">
+              <div class="profile-image">
+                <img src="./img/team/photos/2017/williams.jpg">
+              </div>
+
+              <table class="table table-hover">
+                <tbody>
+                  <thead>
+                    <th colspan="2">Drivetrain Subsystem Leader</th>
+                  </thead>
+
+                  <tr>
+                    <td>Year</td>
+                    <td>Junior</td>
+                  </tr>
+
+                  <tr>
+                    <td>Hometown</td>
+                    <td>Belleville, Illinois</td>
+                  </tr>
+
+                  <tr>
+                    <td>NetID</td>
+                    <td><a href="./contact.php?drivetrain=y">bjwllms2</a></td>
+                  </tr>
+
+                </tbody>
+              </table>
+
+              <div class="well">
+                <p>Ben is a junior in mechanical engineering and has been involved with the team since freshman year. As drivetrain leader his main focus has been on the integration of a bosch abs unit, new pedal box, and improvement of simulation standards and practices. Outside of Formula he enjoys golfing, sailing, and going to the gym. He hopes to work in the automotive industry after graduating.</p>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div class="col-md-4">
+          <div class="panel panel-default">
+            <div class="panel-heading">
+              <strong>James Schackmuth</strong>
+            </div>
+
+            <div class="panel-body">
+              <div class="profile-image">
+                <img src="./img/team/photos/2017/schackmuth.jpg">
+              </div>
+
+              <table class="table table-hover">
+                <tbody>
+                  <thead>
+                    <th colspan="2">Chassis Subsystem Leader</th>
+                  </thead>
+
+                  <tr>
+                    <td>Year</td>
+                    <td>Junior</td>
+                  </tr>
+
+                  <tr>
+                    <td>Hometown</td>
+                    <td>Orland Park, Illinois</td>
+                  </tr>
+
+                  <tr>
+                    <td>NetID</td>
+                    <td><a href="./contact.php?chassis=y">schckmt2</a></td>
+                  </tr>
+
+                </tbody>
+              </table>
+
+              <div class="well">
+                <p>James is a junior at UIUC in mechanical engineering. He has been on chassis team for all three years at the university. After graduating, James hopes to work in the automotive industry. In what little time is afforded him, James can be found playing hockey.</p>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div class="col-md-4">
+          <div class="panel panel-default">
+            <div class="panel-heading">
+              <strong>Jake Shindelar</strong>
+            </div>
+
+            <div class="panel-body">
+              <div class="profile-image">
+                <img src="./img/team/photos/2017/shindelar.jpg">
+              </div>
+
+              <table class="table table-hover">
+                <tbody>
+                  <thead>
+                    <th colspan="2">Engine Subsystem Lead</th>
+                  </thead>
+
+                  <tr>
+                    <td>Year</td>
+                    <td>Senior</td>
+                  </tr>
+
+                  <tr>
+                    <td>Hometown</td>
+                    <td>Barrington, Illinois</td>
+                  </tr>
+
+                  <tr>
+                    <td>NetID</td>
+                    <td><a href="./contact.php?engine=y">shindel2</a></td>
+                  </tr>
+
+                </tbody>
+              </table>
+
+              <div class="well">
+                <p>Jake is a senior in mechanical engineering and has worked with the engine subsystem since his freshman year. He also has a large manufacturing role for the team and is responsible for much of the CNC machining projects. After graduation he will be starting his full time career at Fiat Chrysler Automobiles in Auburn Hills Michigan.</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div class="row">
+        <div class="col-md-4">
+          <div class="panel panel-default">
+            <div class="panel-heading">
+              <strong>Alex Wei</strong>
+            </div>
+
+            <div class="panel-body">
+              <div class="profile-image">
+                <img src="./img/team/photos/2017/wei.jpg">
+              </div>
+
+              <table class="table table-hover">
+                <tbody>
+                  <thead>
+                    <th colspan="2">CAD Administrator &amp; Media Specialist</th>
+                  </thead>
+
+                  <tr>
+                    <td>Year</td>
+                    <td>Senior</td>
+                  </tr>
+
+                  <tr>
+                    <td>Hometown</td>
+                    <td>Queens, New York</td>
+                  </tr>
+
+                  <tr>
+                    <td>NetID</td>
+                    <td><a href="./contact.php">awei3</a></td>
+                  </tr>
+
+                </tbody>
+              </table>
+
+              <div class="well">
+                <p>Alex is a senior at UIUC studying Mechanical Engineering and minoring in Computer Science. He has a sharp interest in understanding how Creo works, and likes figuring out how to improve assembly modeling across the car.  When he isn't behind a 3D mouse or camera, he likes cycling and listening to electronic music.</p>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div class="col-md-4">
+          <div class="panel panel-default">
+            <div class="panel-heading">
+              <strong>David Thunga</strong>
+            </div>
+
+            <div class="panel-body">
+              <div class="profile-image">
+                <img src="./img/team/photos/2017/thunga.jpg">
+              </div>
+
+              <table class="table table-hover">
+                <tbody>
+                  <thead>
+                    <th colspan="2">Aerodynamics Subsystem Lead</th>
+                  </thead>
+
+                  <tr>
+                    <td>Year</td>
+                    <td>Senior</td>
+                  </tr>
+
+                  <tr>
+                    <td>Hometown</td>
+                    <td>Libertyville, Illinois</td>
+                  </tr>
+
+                  <tr>
+                    <td>NetID</td>
+                    <td><a href="./contact.php?aerodynamics=y">thunga2</a></td>
+                  </tr>
+
+                </tbody>
+              </table>
+
+              <div class="well">
+                <p>David Thunga is a senior in mechanical engineering at UIUC.  When he's not eating or sleeping he enjoys playing guitar, going to the gym, and spending time with his family.  After graduation, he plans to pursue a Master's Degree in Aerospace Engineering.</p>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div class="col-md-4">
+          <div class="panel panel-default">
+            <div class="panel-heading">
+              <strong>Noah Lopez</strong>
+            </div>
+
+            <div class="panel-body">
+              <div class="profile-image">
+                <img src="./img/team/photos/2017/lopez.jpg">
+              </div>
+
+              <table class="table table-hover">
+                <tbody>
+                  <thead>
+                    <th colspan="2">Suspension Subsystem Lead</th>
+                  </thead>
+
+                  <tr>
+                    <td>Year</td>
+                    <td>Junior</td>
+                  </tr>
+
+                  <tr>
+                    <td>Hometown</td>
+                    <td>Urbana, Illinois</td>
+                  </tr>
+
+                  <tr>
+                    <td>NetID</td>
+                    <td><a href="./contact.php?suspension=y">nblopez</a></td>
+                  </tr>
+
+                </tbody>
+              </table>
+
+              <div class="well">
+                <p></p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div class="content container">
       <h1>2015-2016 Team Leaders</h1>
 
       <div class="row">
