e7a0044418c8d8789c197f4334c9ce12eaefb423
vmakar <vmakar2@illinois.edu>
2017-12-22 16:01:45 -0600

    sticky navbar on home page
    Added js to implent a sticky navbar on scrolling for the home page

--- js/index.js
+++ js/index.js
@@ -19,3 +19,28 @@ Veloce.applyActive = function() {
 $(function() {
   Veloce.applyActive();
 });
+
+$(document).ready(function() {
+
+
+var topHeight = $('#header').offset().top;     
+
+$(window).scroll(function() {                  
+    var currentScroll = $(window).scrollTop(); 
+    if (currentScroll >= topHeight) {  
+
+        $('#header').css({                     
+            position: 'fixed',
+            top: '0',
+            left: '0'
+        });
+    } else {                                  
+        $('#header').css({                    
+            position: 'static'
+        });
+     
+    }
+
+});
+
+});
